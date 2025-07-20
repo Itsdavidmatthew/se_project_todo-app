66c09254157e626e8a6ae573bf76534100333ac1
davidbedoya <davidbedoya1000@gmail.com>
2025-07-20 11:42:46 -0400

    Project 8 Simple ToDo App 2nd  Submission

--- components/Popup.js
+++ components/Popup.js
@@ -1,35 +1,28 @@
 class Popup {
   constructor({ popupSelector }) {
     this._popupElement = document.querySelector(popupSelector);
-    this._addTodoCloseBtn = this._popupElement.querySelector(".popup__close");
+    this._handlEscapeBind = (evt) => {
+      this._handleEscapeClose(evt);
+    };
   }
 
   _handleEscapeClose(evt) {
     if (evt.key === "Escape") {
-      console.log("esape was pressed");
       this.close();
     }
   }
 
   open() {
     this._popupElement.classList.add("popup_visible");
-    document.addEventListener("keyup", (evt) => {
-      this._handleEscapeClose(evt);
-    });
+    document.addEventListener("keyup", this._handlEscapeBind);
   }
 
   close() {
     this._popupElement.classList.remove("popup_visible");
-    document.removeEventListener("keyup", (evt) => {
-      this._handleEscapeClose(evt);
-    });
+    document.removeEventListener("keyup", this._handlEscapeBind);
   }
 
   setEventListeners() {
-    // this._addTodoCloseBtn.addEventListener("click", (evt) => {
-    //   this.close();
-    // });
-
     this._popupElement.addEventListener("mousedown", (evt) => {
       if (
         evt.target.classList.contains("popup__close") ||
